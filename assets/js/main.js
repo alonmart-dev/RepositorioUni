9b0e2de02cb2811d0d918b454c43f7f705bff6aa
AlonsoMartin <alonmart.dev@gmail.com>
2024-10-17 16:35:06 +0200

    Ejercicios(17/10/24)

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,11 +24,35 @@ window.onload = function () {
 
 function Alonso(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+}
 
 function alonsoColor(elem){
     const r = Alonso(0, 255);
     const g = Alonso(0, 255);
     const b = Alonso(0, 255);
     elem.style.color = "rgb("+r+","+g+","+b+")";
+}
+
+function cambiaColor(elem){
+    const r = Alonso(0, 255);
+    const g = Alonso(0, 255);
+    const b = Alonso(0, 255);
+    elem.style.backgroundColor = "rgb("+r+","+g+","+b+")";
+}
+
+function vuelveColor(elem){
+    elem.style.backgroundColor = "";
+}
+
+
+function cambia1(elem){
+    elem.innerHTML = Math.random();
+}
+
+function cambia2(elem){
+    document.getElementsByTagName("main")[0].style.backgroundImage = "url('./assets/img/crayones_jpg.jpg')";
+}
+
+function cambia3(elem){
+    document.getElementsByClassName("botones")[0].style.flexDirection = "row-reverse";
 }
