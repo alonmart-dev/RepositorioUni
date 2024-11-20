fa235d30d4f89244c335727aef34d5c2ee719772
AlonsoMartin <alonmart.dev@gmail.com>
2024-11-20 22:52:59 +0100

    Robot 20/11/24

--- ConDavid/InicioJSCreativo.js
+++ ConDavid/InicioJSCreativo.js
@@ -1,45 +1,52 @@
 function setup(){
     createCanvas(600, 600);
 
-    strokeWeight(0);
+}
 
-    fill(255, 159, 0); //naranja claro
-    circle(281, 60, 38);
-
-    fill(230, 144, 0);  //naranja oscuro
+function draw(){
+    background(235);
+    strokeWeight(0); 
     
+    fill(255, 159, 0); //naranja claro
+
+    circle(285, 61, 38);
+    circle(220, 150, 50);
+    circle(350, 150, 50);
 
 
     fill(54, 71, 80); //azul oscuro
 
-
-    rect(215, 105, 141, 130);
-
-    fill(205, 205, 205); //gris claro
-
-
-    rect(274, 78, 14, 28);
-
-
+    rect(215, 105, 141, 130); 
+    rect(230, 395, 24, 70);  
+    rect(320, 395, 24, 70);
+    rect(115, 265, 25, 50, 10);
+    rect(430, 265, 25, 50, 10);  
 
     fill(127, 143, 150); //azul claaro
 
+    rect(140, 285, 7, 10);
+    rect(424, 285, 7, 10);
+    
+    fill(205, 205, 205); //gris claro
 
+    rect(278, 78, 14, 28);
+    circle(315, 140, 20);
+    circle(260, 140, 20);
+    rect(253, 185, 70, 10);
+    circle(170, 288, 50);
+    circle(400, 288, 50);
+    rect(120, 315, 15, 50, 5);
+    rect(435, 315, 15, 50, 5);  
+    
+    fill(255, 159, 0); //naranja claro
 
-    /*
-    fill(255, 0, 0); //relleno(r, g, b)
-    stroke(0, 50, 0); //borde(r, g, b)
+    rect(170, 235, 230, 100, 30);
+    rect(307, 460, 50, 35, 20, 20, 5, 5);  
+    rect(217, 460, 50, 35, 20, 20, 5, 5);  
 
-    strokeWeight(2); //grosor
+    fill(230, 144, 0);  //naranja oscuro
 
-    ellipse(400, 300, 50, 50); //x, y, ancho, alto
+    rect(200, 335, 170, 60);
 
-    fill(20, 100, 150); //relleno(r, g, b)
-
-    circle(20, 20, 20); //x, y, radio
-    rect(50, 50, 120, 170); //x, y, ancho, alto
-    line(20, 20, 400, 300);  //x1, y1, x2, y2
-    point(400, 400); //x, y
-    */
     
 }
