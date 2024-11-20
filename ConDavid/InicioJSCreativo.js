260a86aad32b247eb285ab363dda70d7bcef7d02
AlonsoMartin <alonmart.dev@gmail.com>
2024-11-20 12:32:23 +0100

    InicioP5Canvas

---
+++ ConDavid/InicioJSCreativo.js
@@ -0,0 +1,45 @@
+function setup(){
+    createCanvas(600, 600);
+
+    strokeWeight(0);
+
+    fill(255, 159, 0); //naranja claro
+    circle(281, 60, 38);
+
+    fill(230, 144, 0);  //naranja oscuro
+    
+
+
+    fill(54, 71, 80); //azul oscuro
+
+
+    rect(215, 105, 141, 130);
+
+    fill(205, 205, 205); //gris claro
+
+
+    rect(274, 78, 14, 28);
+
+
+
+    fill(127, 143, 150); //azul claaro
+
+
+
+    /*
+    fill(255, 0, 0); //relleno(r, g, b)
+    stroke(0, 50, 0); //borde(r, g, b)
+
+    strokeWeight(2); //grosor
+
+    ellipse(400, 300, 50, 50); //x, y, ancho, alto
+
+    fill(20, 100, 150); //relleno(r, g, b)
+
+    circle(20, 20, 20); //x, y, radio
+    rect(50, 50, 120, 170); //x, y, ancho, alto
+    line(20, 20, 400, 300);  //x1, y1, x2, y2
+    point(400, 400); //x, y
+    */
+    
+}
